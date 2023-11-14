ca4b08362ae63f0a387ff306f19851236ed784e9
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-14 12:54:21 +0530

    password verification

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 
 
@@ -10,10 +11,23 @@ def register(request):
         lastname = request.POST['Lastname']
         email = request.POST['email']
         password = request.POST['password1']
-        user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname, email=email,
-                                        password=password)
-        user.save()
-        print('user created')
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'username already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email already taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, first_name=firstname, last_name=lastname,
+                                                email=email, password=password)
+                user.save()
+                print('user created')
+                # return redirect('/')
+        else:
+            print('password not match')
+            return redirect('register')
         return redirect('/')
     else:
         return render(request, 'register.html')
