9dbc43fc11733afc6115ef585c7ce8b3ec0d2dcd
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-14 19:58:30 +0530

    login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,21 @@ from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials..!')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -32,3 +47,8 @@ def register(request):
         return redirect('/')
     else:
         return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
